298081a3bbc5a1f775dc507b6c7acf97f4f35585
Cameron Steele <cam.steeleis@gmail.com>
2020-02-27 09:27:38 -0600

    Inital Commit

---
+++ Assets/Restart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Gamekit2D.InventoryController PlayerInv;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if(PlayerInv.HasItem("Key1") && PlayerInv.HasItem("Key2") && PlayerInv.HasItem("Key3"))
+            {
+                this.RestartLevel();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+            this.RestartLevel();
+           
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
